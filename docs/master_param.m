42691cce20f5836ace07049ed1c5795d9a6f7b44
bHimes <himes.benjamin@gmail.com>
2017-12-06 20:30:28 -0500

    next big version change
    Former-commit-id: f69052e3d14a15d4fbd90fbce430784c0ae4007d

--- docs/master_param.m
+++ docs/master_param.m
@@ -1,77 +1,76 @@
 % This is a comment
 % Inline comments will break the parser.
 
-%%%% TODO
-% pixel size is specified in three places --> one
-% Simplify mVals/SamplingRate
-
 
 % String to name the structure that contains all of the metadata, projectName
-subTomoMeta=rln_tutorial_1
+subTomoMeta=emClarity_tutorial
 
+fastScratchDisk=fastScratchDisk=/local/cache
 
-% Number of GPUS, either a list [1,3] or one int indicating a range 1:int
-nGPUs=1
-% 1 use as much memory as possible | 2 conserve memory. Always try to run in 1
-% but often at full sampling 2 is necessary.
-avgMemory=1
+% Number of GPUS
+nGPUs=4
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%    Mask parameters    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-NoA_mType=sphere
-Pca_mType=sphere
+% The particle radius in x,y,z Angstrom, smallest value to contain particle. 
+% For particles in a lattice, neighboring particles can be used in alignment
+% by specifying a larger mask size, but this paramter must correspond to your
+% target, a cetral hexamer of capsid proteins for example.
+particleRadius=[160,160,160]
+
+Ali_mType=sphere
 Cls_mType=sphere
-Raw_mType=sphere
-Fsc_mType=sphere
-Kms_mType=sphere
 
-Fsc_shapeMask=0
-Pca_shapeMask=0
-Raw_shapeMask=0
 
-% mask size, radius, center - note here mask size is ignored, and the window size
-% is used insead. Note that each is a 3 x 3 matrix
-NoA_mVals=[156,156,156; 84,84,84; 0,0,0]
-Pca_mVals=[156,156,156; 84,84,84; 0,0,0]
-Cls_mVals=[156,156,156; 84,84,84; 0,0,0]
-Raw_mVals=[156,156,156; 84,84,84; 0,0,0]
-Fsc_mVals=[156,156,156; 84,84,84; 0,0,0]
-Kms_mVals=[156,156,156; 84,84,84; 0,0,0]
+% mask radius and center - and center in Angstrom. Mask size is determined 
+% large enough to contain delocalized signal, proper apodization, and to 
+% avoid wraparound error in cross-correlation.
+Ali_mRadius=[180,180,180]
+Ali_mCenter=[0,0,0]
+Cls_mRadius=[180,180,180]
+Cls_mCenter=[0,0,0 ]
 
 
 % Sampling rate
-% Note that all pixel values should be entered at full binning 
-NoA_samplingRate=4
-Pca_samplingRate=4
+Ali_samplingRate=4
 Cls_samplingRate=4
-Raw_samplingRate=4
-Kms_samplingRate=4
-Fsc_samplingRate=4
-Ref_samplingRate=4
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%    Advanced options    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%% Tomo-constrained projection refinement parameters    %%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-scaleCalcSize=2
-flgReWeight=1
-flgQualityWeight=1
-flgPrecision=single
-interpolationOrder=1
-flgCones=0
-flgClassify=0
-flgMultiRefAlignment=0
-flgRotAvgRef=0
-flgCenterRefCOM=1
-flgSymmetrizeSubTomos=0
-duplicateRadius=8
-duplicateSampling=3
-flgCCCcutoff=0.00
-removeBottomPercent=0.0
+
+tomoCprLowPass=18
+
+
+tomoCprDefocusRefine=1
+tomoCprDefocusRange=500e-9; 
+tomoCprDefocusStep=20e-9;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%    subTomogram alignment           %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Raw_className=0
+Raw_classes_odd=[0;1.*ones(2,1)]
+Raw_classes_eve=[0;1.*ones(2,1)]
+
+Raw_angleSearch=[16,4,0,0]
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%    Template matching parameters    %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+Tmp_samplingRate=5
+Tmp_threshold=400
+Tmp_angleSearch=[180,12,180,12,0]
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -105,37 +104,26 @@ Cls_className=9
 Cls_classes_odd=[1:9;1.*ones(1,9)]
 Cls_classes_eve=[1:9;1.*ones(1,9)]
 
-Cls_peakSearch=[30,30,24]
-Cls_centerOfMass=[2,2,2]
+
+
 Cls_angleSearch=[3,1,4,0.5]
 
 % The out of plane inc must be a factor of the range, however, the in plane does 
 % not need to be, and should be chosen to avoid symmetry related peaks.
 
  
-Raw_className=0
-Raw_classes_odd=[0;1.*ones(2,1)]
-Raw_classes_eve=[0;1.*ones(2,1)]
-
-Raw_peakSearch=[64,64,64]
-Raw_centerOfMass=[3,3,3]
-Raw_angleSearch=[0,0,12,3,0]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%    FSC Paramters    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Fsc_withChimera=1
+
 % References to compare, sorted by order
 Fsc_refVector_odd=[1]
 Fsc_refVector_eve=[1]
-% Alignment parameters to bring the two half sets into register
-Fsc_peakSearch=[84,84,84]
-Fsc_centerOfMass=[3,3,3]
-Fsc_angleSearch=[3,0.5,3,0.3]
 
 
-Fsc_bfactor=40
+Fsc_bfactor=10
 
 % For very tightly packed subTomos set to 1 to avoid mixing halfsets
 % form overlaping peripheral density.
@@ -177,31 +165,6 @@ flgWedgeComparison=0
 flgWMDs=3
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%    Template matching parameters    %%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-Tmp_samplingRate=6
-Tmp_threshold=400
-Tmp_targetSize=[512,512,512]
-
-% lattice param is ~ 120A for trimer, 69A for unitcell
-Tmp_latticeRadius=[160,160,160]
-Tmp_angleSearch=[180,12,180,12,0]
-Tmp_xcfScale=xcf
-
-xfcSize=[256,256,256]
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%% Tomo-particle polishing parameters    %%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-mapBackIter=0
-mapBackLowPass=12
-mapBackRePrjSize=128
 
 
 
@@ -210,15 +173,6 @@ mapBackRePrjSize=128
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%   3d CTF paramters    %%%%%%%%%%
-
-% a fraction of the average X,Y dimension to minimally construct to allow for 
-% accurate r-weighting. 
-flgSlabPad=0.25
-% dimension perpendicular to the tilt-axis to pad to during ctf multiplication.
-flgCtfStripWidth=2048
-% Used to deterine slab height and strip width during ctf correction
-defocusErrorEst=30e-9
 
 
 %%%%%%%%%%   Microscope parameters     %%%%%%%%%%
@@ -234,17 +188,17 @@ Cs=2.7e-3
 % Accelerating voltage
 VOLTAGE=300e3
 % Percent amplitude contrast
-AMPCONT=0.085
+AMPCONT=0.10
 
 % search range - generally safe to test a wide range
-defEstimate=4e-6
-defWindow=2.5e-6
+defEstimate=3.5e-6
+defWindow=2e-6
 % The PS is considered from the lower resolution inflection point
 % past the first zero to this cutoff resolution
-defCutOff=8e-10
+defCutOff=7e-10
 
 % Total dose in electron/A^2, assumed constant rate
-CUM_e_DOSE=100
+CUM_e_DOSE=60
 % Gold fiducial diameter
 beadDiameter=10e-9
 
@@ -255,8 +209,7 @@ astigStep=150e-10
 coarseAngStep=10
 fineAngStep=0.5
 
-% numWorkers only affects the interpolation of the stack (spline on cpu)
-numWorkers=7
+
 
 % The following can be adjusted but be careful
 
@@ -274,9 +227,33 @@ tileOverlap=4
 % doesn't really speed things up so don't bother
 paddedSize=1536
 
-%%%%%%%%%%%%%%%%%%    Developmental Parameters    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Rln_mVals=[160,160,120; 48,48,40; 0,0,0]
-Rln_samplingRate=1
-Rln_tomogramCp=0
-% 0 - nothing,1 - mv, 2 - cp , 3 - ln symbolic
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%    Advanced options    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%   3d CTF paramters    %%%%%%%%%%
+
+% a fraction of the average X,Y dimension to minimally construct to allow for 
+% accurate r-weighting. 
+flgSlabPad=0.25
+% dimension perpendicular to the tilt-axis to pad to during ctf multiplication.
+flgCtfStripWidth=1536
+
+
+flgPrecision=single
+flgCones=2
+flgClassify=0
+flgSymmetrizeSubTomos=0
+duplicateRadius=8
+duplicateSampling=4
+flgCCCcutoff=0.00
+removeBottomPercent=0.0
+flgMultiRefAlignment=0
+flgRotAvgRef=0
+
+experimentalOpts=[1,1,1,2,1,1,1,3,6,0]
+
+
 
