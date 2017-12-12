582af79da72d48a95ee87efb9f03cb10be649a3c
Benjamin Himes <himesb@london.hhmi.org>
2017-12-12 09:14:07 -0500

    important change to logfile creation, evalMask in tomoCPR, and division of labor among GPUs
    Former-commit-id: fe81ab52bc077f0d6738b2bf7ad47b4edefe59fa

--- update_emClarity.sh
+++ update_emClarity.sh
@@ -3,10 +3,13 @@
 # For non-developers git & and merging is overkill. Save a clean copy so that 
 # original paths are preserved in custom shell script then push back
 
-emClarityRoot=$(awk '{if(/^emClarity_ROOT=/) print $0}' emClarity)
-mcrRoot=$(awk '{if(/^MCR_BASH=/) print $0}' emClarity)
+emClarityRoot16=$(awk '{if(/^emClarity_ROOT=/) print $0}' emClarity_16b)
+mcrRoot16=$(awk '{if(/^MCR_BASH=/) print $0}' emClarity_16b)
 
-if [[ $mcrRoot && $emClarityRoot ]] ; then
+emClarityRoot17=$(awk '{if(/^emClarity_ROOT=/) print $0}' emClarity_17b)
+mcrRoot17=$(awk '{if(/^MCR_BASH=/) print $0}' emClarity_17b)
+
+if [[ $mcrRoot16 && $emClarityRoot16 ]] ; then
   echo "Found root dirs, moving on."
 else
   echo "Did not find root dirs, exiting."
@@ -19,19 +22,27 @@ mv emClarity emClarity~
 
 git clone --depth=1 https://github.com/bHimes/emClarity.git
 
+# update 16b
+awk -v emCR="${emClarityRoot16}" '{if(/^emClarity_ROOT=/) print emCR; else print $0}' emClarity/emClarity_16b > emClarity/emClarity.tmp1
+awk -v mcrR="${mcrRoot16}" '{if(/^MCR_BASH=/) print mcrR; else print $0}' emClarity/emClarity.tmp1 > emClarity/emClarity.tmp2
 
-awk -v emCR="${emClarityRoot}" '{if(/^emClarity_ROOT=/) print emCR; else print $0}' emClarity/emClarity > emClarity/emClarity.tmp1
-awk -v mcrR="${mcrRoot}" '{if(/^MCR_BASH=/) print mcrR; else print $0}' emClarity/emClarity.tmp1 > emClarity/emClarity.tmp2
+mv emClarity/emClarity.tmp2 emClarity/emClarity_16b
 
-mv emClarity/emClarity.tmp2 emClarity/emClarity
+rm emClarity/emClarity.tmp1
+
+# update 17b
+awk -v emCR="${emClarityRoot17}" '{if(/^emClarity_ROOT=/) print emCR; else print $0}' emClarity/emClarity_16b > emClarity/emClarity.tmp1
+awk -v mcrR="${mcrRoot17}" '{if(/^MCR_BASH=/) print mcrR; else print $0}' emClarity/emClarity.tmp1 > emClarity/emClarity.tmp2
+
+mv emClarity/emClarity.tmp2 emClarity/emClarity_17b
 
 rm emClarity/emClarity.tmp1
 
 cd emClarity
 
-chmod a=wrx emClarity
-
-emClarity check
+chmod a=wrx emClarity_16b
+chmod a=wrx emClarity_17b
+#emClarity check
 
 echo "Please look at the emClarity check log to make sure all is well."
 echo "Then remove the old install which is currently at ../emClarity~"
