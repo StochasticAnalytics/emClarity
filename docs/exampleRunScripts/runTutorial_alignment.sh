1aba28436d717aaa45969970d262da68216742c3
Benjamin Himes <himesb@london.hhmi.org>
2017-12-21 14:52:14 -0500

    tomoCPR simplified, whiten noise spectrum in alignment, and parallelize recon in 3dCTF
    Former-commit-id: 6af0ab08f71835110f1a599598dd977564e1d750

--- docs/exampleRunScripts/runTutorial_alignment.sh
+++ docs/exampleRunScripts/runTutorial_alignment.sh
@@ -24,28 +24,26 @@ fi # end of skip, if you want to skip a section, just move this line to that poi
       ST='RawAlignment'
     fi
 
-    if [[ $i -ge 0 ]] ; then   emClarity avgST param${i}.m ${i} $ST ; fi
+    if [[ $i -ge 0 ]] ; then   emClarity avg param${i}.m ${i} $ST ; fi
       
-    if [[ $i -ge 0 ]] ; then  emClarity alignRawST param${i}.m ${i}; fi
+    if [[ $i -ge 0 ]] ; then  emClarity alignRaw param${i}.m ${i}; fi
       
   done
 
    
-   emClarity tomoCPR param2.m 2 cycle002_emClarity_tutorial_class0_REF_ODD.mrc 0 16 1 GPU ${nCores}
+   emClarity tomoCPR param2.m 2 4
   
    # We need to re-create the aligned stacks with the updated alignments from tomoCPR
    emClarity ctf update param3.m -1 full
-   # The meta data needs to be updated
-   emClarity geometry param3.m 3 TiltAlignment UpdateTilts [0,0,0] STD
-   # The newly aligned stacks and the new local alignment info are used to reconstruct
+
    emClarity ctf 3d param3.m
 
   
   # Do the same thing, but now at a binning of 3, adding a duplicate removal step.
   for i in 3 4 5; do
-    if [[ $i -ge 3 ]] ; then  emClarity avgST param${i}.m ${i} RawAlignment ; fi
+    if [[ $i -ge 3 ]] ; then  emClarity avg param${i}.m ${i} RawAlignment ; fi
     
-    if [[ $i -ge 3 ]] ; then  emClarity alignRawST param${i}.m ${i} ; fi
+    if [[ $i -ge 3 ]] ; then  emClarity alignRaw param${i}.m ${i} ; fi
     
   done
 
@@ -53,11 +51,9 @@ fi # end of skip, if you want to skip a section, just move this line to that poi
   # any duplicate sub-tomogramnms
   emClarity removeDuplicates param5.m 5 
 
-  emClarity tomoCPR param5.m 5 cycle005_emClarity_tutorial_class0_REF_ODD.mrc 0 32 1 GPU ${nCores}
+  emClarity tomoCPR param5.m 5 4
   
-  emClarity ctf update param6.m -1 full
-    
-  emClarity geometry param6.m 6 TiltAlignment UpdateTilts [0,0,0] STD
+  emClarity ctf update param6.m -1 full   
   
   emClarity ctf 3d param6.m
 
@@ -65,17 +61,15 @@ fi # end of skip, if you want to skip a section, just move this line to that poi
 
   # Again but now at a binning of 2
   for i in 6 7 8 ; do
-    if [[ $i -ge 6 ]] ; then  emClarity avgST param${i}.m ${i} RawAlignment ; fi
+    if [[ $i -ge 6 ]] ; then  emClarity avg param${i}.m ${i} RawAlignment ; fi
     
-    if [[ $i -ge 6 ]] ; then  emClarity alignRawST param${i}.m ${i} ; fi
+    if [[ $i -ge 6 ]] ; then  emClarity alignRaw param${i}.m ${i} ; fi
     
   done
  
-  emClarity tomoCPR param8.m 8 cycle008_emClarity_tutorial_class0_REF_ODD.mrc 0 64 1 GPU ${nCores}
+  emClarity tomoCPR param8.m 8 4
   
-  emClarity ctf update param9.m -1 full
-    
-  emClarity geometry param9.m 9 TiltAlignment UpdateTilts [0,0,0] STD
+  emClarity ctf update param9.m -1 full  
   
   emClarity ctf 3d param9.m 
 
@@ -84,14 +78,14 @@ fi # end of skip, if you want to skip a section, just move this line to that poi
 
   for i in 9 10 11 12  ; do
 
-    if [[ $i -ge 9 ]] ; then  emClarity avgST param${i}.m ${i} RawAlignment ; fi
+    if [[ $i -ge 9 ]] ; then  emClarity avg param${i}.m ${i} RawAlignment ; fi
     
-    if [[ $i -ge 9 ]] ; then  emClarity alignRawST param${i}.m ${i} ; fi
+    if [[ $i -ge 9 ]] ; then  emClarity alignRaw param${i}.m ${i} ; fi
     
   done
 
   # A regular average to get a final estimate of the SSNR
-  emClarity avgST param13.m 13 RawAlignment
+  emClarity avg param13.m 13 RawAlignment
   # Align and merge the two half-sets applying requested b-factors.
-  emClarity avgST param13.m 13 FinalAlignment
+  emClarity avg param13.m 13 FinalAlignment
 
